d7d294801d7ba6915e24096295e964b3048cd7c7
tuguscript <tuguscript@gmail.com>
2018-09-22 00:15:39 -0700

    added material theme

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,9 +2,21 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 
 import Header from './header'
 import './layout.css'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: '#ff4400',
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+  },
+})
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -22,23 +34,31 @@ const Layout = ({ children }) => (
         <Helmet
           title={data.site.siteMetadata.title}
           meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
+            {
+              name: 'description',
+              content: 'Sample',
+            },
+            {
+              name: 'keywords',
+              content: 'sample, something',
+            },
           ]}
         >
           <html lang="en" />
-        </Helmet>
+        </Helmet>{' '}
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div>
+        <MuiThemeProvider theme={theme}>
+          <div
+            style={{
+              margin: '0 auto',
+              maxWidth: 960,
+              padding: '0px 1.0875rem 1.45rem',
+              paddingTop: 0,
+            }}
+          >
+            {children}
+          </div>
+        </MuiThemeProvider>
       </>
     )}
   />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'gatsby'
+import 'typeface-roboto'
+import Typography from '@material-ui/core/Typography'
+import { AccessAlarm } from '@material-ui/icons'
+import Icon from '@material-ui/core/Icon'
+import { loadCSS } from 'fg-loadcss/src/loadCSS'
+import Button from '@material-ui/core/Button'
 
 import Layout from '../components/layout'
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+class IndexPage extends Component {
+  componentDidMount = () => {
+    loadCSS(
+      'https://use.fontawesome.com/releases/v5.1.0/css/all.css',
+      document.querySelector('#insertion-point-jss')
+    )
+  }
+
+  render() {
+    return (
+      <Layout>
+        <h1> Hi people </h1> <p> Welcome to your new Gatsby site. </p>
+        <p> Now go build something great. </p>
+        <Typography variant="display4" gutterBottom>
+          Display 4
+        </Typography>
+        <AccessAlarm />
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
+        <Icon className="fa fa-plus-circle" />
+        <Link to="/page-2/"> Go to page 2 </Link>
+      </Layout>
+    )
+  }
+}
 
 export default IndexPage
