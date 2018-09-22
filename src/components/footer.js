1026db81127edcccf8aa070b95975b8ed3144654
tuguscript <tuguscript@gmail.com>
2018-09-22 01:38:31 -0700

    added svgs in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,21 +1,46 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Icon from '@material-ui/core/Icon'
-import ReactSVG from '../images/svg/react.svg'
+import GatsbyIcon from '../images/svg/gatsby.svg'
+import ReactIcon from '../images/svg/react.svg'
+import GraphqlIcon from '../images/svg/graphql.svg'
+import JssIcon from '../images/svg/jss.svg'
+import MaterialUiIcon from '../images/svg/material-ui.svg'
+import WebpackIcon from '../images/svg/webpack.svg'
+import BabelIcon from '../images/svg/babel.svg'
+import NetlifyIcon from '../images/svg/netlify.svg'
 
-const Footer = () => (
-  <footer
-    style={{
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div>
-      <ReactSVG />
-      <Icon className="fab fa-github" />
-      <Icon className="fab fa-linkedin-in" />
-      <Icon className="fab fa-twitter" />
-    </div>
-  </footer>
-)
+const Footer = () => {
+  const items = [
+    { name: 'gatsby', url: 'https://www.gatsbyjs.org/', comp: GatsbyIcon },
+    { name: 'react', url: 'https://reactjs.org/', comp: ReactIcon },
+    { name: 'graphql', url: 'http://graphql.org/', comp: GraphqlIcon },
+    { name: 'jss', url: 'http://cssinjs.org/', comp: JssIcon },
+    {
+      name: 'material-ui',
+      url: 'https://material-ui-next.com/',
+      comp: MaterialUiIcon,
+    },
+    { name: 'webpack', url: 'https://webpack.js.org/', comp: WebpackIcon },
+    { name: 'babel', url: 'https://babeljs.io/', comp: BabelIcon },
+    { name: 'netlify', url: 'https://www.netlify.com/', comp: NetlifyIcon },
+  ]
+  return (
+    <footer
+      style={{
+        marginBottom: '1.45rem',
+      }}
+    >
+      <div>
+        {items.map(item => (
+          <img src={item.comp} alt="" srcset="" />
+        ))}
+        <Icon className="fab fa-github" />
+        <Icon className="fab fa-linkedin-in" />
+        <Icon className="fab fa-twitter" />
+      </div>
+    </footer>
+  )
+}
 
 export default Footer
