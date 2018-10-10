68bcec14391f393e94f296a59cee456a1964e972
tuguscript <tuguscript@gmail.com>
2018-10-10 15:55:36 -0700

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -160,6 +160,33 @@ class IndexPageUnstyled extends Component {
             Contact{' '}
           </Typography>{' '}
           <div>
+            <form method="post" action="#">
+              <div className="field half first">
+                <label htmlFor="name">Name</label>
+                <input type="text" name="name" id="name" />
+              </div>
+              <div className="field half">
+                <label htmlFor="email">Email</label>
+                <input type="text" name="email" id="email" />
+              </div>
+              <div className="field">
+                <label htmlFor="message">Message</label>
+                <textarea name="message" id="message" rows="6" />
+              </div>
+              <ul className="actions">
+                <li>
+                  <input
+                    type="submit"
+                    value="Send Message"
+                    className="special"
+                  />
+                </li>
+                <li>
+                  <input type="reset" value="Clear" />
+                </li>
+              </ul>
+            </form>
+
             <form
               name="contact"
               method="post"
