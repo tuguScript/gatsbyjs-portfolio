432e52e54ff4701fd89b7df11b69c378ef1c671d
tuguscript <tuguscript@gmail.com>
2018-10-10 14:06:29 -0700

    added Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,27 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    `gatsby-plugin-jss`,
+    'gatsby-plugin-jss',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-127304111-1',
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ['/preview/**', '/do-not-track/me/too/'],
+        // Enables Google Optimize using your container Id
+        optimizeId: 'YOUR_GOOGLE_OPTIMIZE_TRACKING_ID',
+        // Any additional create only fields (optional)
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: 'tugi.me',
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
