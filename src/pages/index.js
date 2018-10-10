6e51a2dd456b8edd94419a896e7032904a6e2b05
tuguscript <tuguscript@gmail.com>
2018-10-10 16:32:38 -0700

    contact form setting

--- src/pages/index.js
+++ src/pages/index.js
@@ -90,7 +90,7 @@ class IndexPageUnstyled extends Component {
   render() {
     let { worksData } = this.state
     let { classes } = this.props
-    const { name, email, message } = this.state;
+    const { name, email, message } = this.state
     return (
       <Layout>
         <section className={classes.page1}>
@@ -184,96 +184,60 @@ class IndexPageUnstyled extends Component {
             Contact{' '}
           </Typography>{' '}
           <div>
-          <form onSubmit={this.handleSubmit} netlify="true">
-          <input type="hidden" name="form-name" value="contact" /> 
-          <p>
-            <label>
-              Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message: <textarea name="message" value={message} onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
-            <form name="contact2" method="POST" netlify>
+            <form onSubmit={this.handleSubmit} netlify="true">
+              <input type="hidden" name="form-name" value="contact" />
               <p>
                 <label>
-                  Your Name: <input type="text" name="name" />
+                  Your Name:{' '}
+                  <input
+                    type="text"
+                    name="name"
+                    value={name}
+                    onChange={this.handleChange}
+                  />
                 </label>
               </p>
               <p>
                 <label>
-                  Your Email: <input type="email" name="email" />
+                  Your Email:{' '}
+                  <input
+                    type="email"
+                    name="email"
+                    value={email}
+                    onChange={this.handleChange}
+                  />
                 </label>
               </p>
               <p>
                 <label>
-                  Your Role:{' '}
-                  <select name="role[]" multiple>
-                    <option value="leader">Leader</option>
-                    <option value="follower">Follower</option>
-                  </select>
-                </label>
-              </p>
-              <p>
-                <label>
-                  Message: <textarea name="message" />
+                  Message:{' '}
+                  <textarea
+                    name="message"
+                    value={message}
+                    onChange={this.handleChange}
+                  />
                 </label>
               </p>
               <p>
                 <button type="submit">Send</button>
               </p>
             </form>
-            <form
-              name="contact1"
-              method="post"
-              data-netlify="true"
-              data-netlify-honeypot="bot-field"
-            >
-              <input type="hidden" name="bot-field" />
-              <div className="field half first">
-                <label htmlFor="name">Name</label>
-                <input type="text" name="name" id="name" />
-              </div>
-              <div className="field half">
-                <label htmlFor="email">Email</label>
-                <input type="text" name="email" id="email" />
-              </div>
-              <div className="field">
-                <label htmlFor="message">Message</label>
-                <textarea name="message" id="message" rows="6" />
-              </div>
-              <ul className="actions">
-                <li>
-                  <input
-                    type="submit"
-                    value="Send Message"
-                    className="special"
-                  />
-                </li>
-                <li>
-                  <input type="reset" value="Clear" />
-                </li>
-              </ul>
-            </form>
 
-            <form
-              name="contact"
-              method="post"
-              data-netlify="true"
-              data-netlify-honeypot="bot-field"
-            >
+            <form onSubmit={this.handleSubmit} netlify="true">
               <input type="hidden" name="bot-field" />
+              <TextField
+                required
+                id="outlined-text-input"
+                label="Name"
+                type="name"
+                name="name"
+                autoComplete="name"
+                margin="normal"
+                variant="filled"
+                fullWidth
+                value={name}
+                onChange={this.handleChange}
+              />
               <TextField
                 required
                 id="outlined-email-input"
@@ -284,8 +248,13 @@ class IndexPageUnstyled extends Component {
                 margin="normal"
                 variant="filled"
                 fullWidth
+                value={email}
+                onChange={this.handleChange}
               />
               <TextField
+                name="message"
+                value={message}
+                onChange={this.handleChange}
                 required
                 id="outlined-full-width"
                 label="Message"
