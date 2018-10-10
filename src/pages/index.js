4f3f1bd0acb7451d3ba6bbddd75995e4a576ffc5
tuguscript <tuguscript@gmail.com>
2018-10-10 11:47:52 -0700

    added one workData

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,7 @@ import injectSheet from 'react-jss'
 import './index.css'
 
 // TODO: on scrollUP, show header
+//google analytics
 // revealjs. everything
 // moon sun icon change Animation
 // email send
