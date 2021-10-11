490aea4527d7ce19ee14bafd424f0d7dc8ff9119
tuguscript <tuguscript@gmail.com>
2021-10-11 15:54:53 -0700

    :zap:

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Tugi`,
+    description: `
+About Tugi, Tuguldur Bayarnyam`,
+    author: `@tuguscript`,
+    siteUrl: `https://tugi.me`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
